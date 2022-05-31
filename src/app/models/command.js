615297ee25b5f36742b60ad746058fa4a0332219
Diego Arruda <arrudadiego45@gmail.com>
2022-05-31 11:36:28 -0300

    feat: creating command model

---
+++ src/app/models/command.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const commandSchema = new Schema({
+  createdAt: Date,
+  table: {
+    type: String,
+    required: true,
+    maxLength: 128,
+  },
+  waiter: {
+    type: String,
+    required: true,
+  },
+  fishingType: String,
+  products: [
+    {
+      name: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      category: String,
+      unitPrice: {
+        type: Number,
+        required: true,
+      },
+      amount: {
+        type: Number,
+        min: 0,
+      },
+    },
+  ],
+  total: Number,
+});
+
+const Command = mongoose.model('Command', commandSchema);
+
+module.exports = Command;
